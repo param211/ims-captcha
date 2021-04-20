a835ba164a410a02813f6ac4a3baa703425c5f6a
param211 <paramchauhan21@gmail.com>
2021-04-20 12:19:10 +0530

    Initial commit

---
+++ captcha.js
@@ -0,0 +1,30 @@
+console.log("YoYo")
+
+async function getapi(url) {
+    
+    // Storing response
+    const response = await fetch(url, {
+        mode: 'cors',
+        headers: {
+          'Access-Control-Allow-Origin':'*'
+        }}
+    );
+    
+    window.frames[0].document.getElementById('cap').value = await response.json();
+}
+
+window.onload = function() {
+
+    var img = window.frames[0].document.getElementById('captchaimg')
+
+    var regexPattern = /\d+/g;
+
+    captchaID = img.src.match(regexPattern)
+
+    apiUrl = "https://server.paramchauhan.com/ims?captcha_id=" + captchaID
+
+    console.log(apiUrl)
+
+    getapi(apiUrl)
+
+}
